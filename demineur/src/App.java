0d84344fe918d07fdcbfe521316d579a0a018b2f
Corentin <corentin.bunaux@gmail.com>
2024-09-10 17:19:55 +0200

    before removing java

--- demineur/src/App.java
+++ demineur/src/App.java
@@ -1,19 +1,39 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
 /**
  * App
+ * 
  * @author Corentin Bunaux
  * @version 0.0
- * This program prints "Hello, World!" to the console.
+ *          This program prints "Hello, World!" to the console.
  */
 
-public class App {
+public class App extends JFrame{
     /**
      * Main method
+     * 
      * @param args not used
      * @throws Exception it's not my problem
      */
-    public static void main(String[] args) throws Exception {
+
+    App() {
         Champ champ = new Champ();
-        champ.init(10, 10,7);
+        champ.init(10, 10, 7);
         champ.display();
+
+        //creation d'un JPanel
+        GUI gui = new GUI();
+        
+        //affectation du JPanel dans la JFrame
+        setContentPane(gui);
+        pack();
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new App();
     }
 }
--- demineur/src/Champ.java
+++ demineur/src/Champ.java
@@ -8,13 +8,11 @@ public class Champ {
     public void display() {
         for (int i = 0; i < this.field.length; i++) {
             for (int j = 0; j < this.field[i].length; j++) {
-                if(this.field[i][j] == -1) {
+                if (this.field[i][j] == -1) {
                     System.out.print("X");
-                }
-                else if(this.field[i][j] == 0) {
+                } else if (this.field[i][j] == 0) {
                     System.out.print(".");
-                }
-                else {
+                } else {
                     System.out.print(this.field[i][j]);
                 }
             }
@@ -23,21 +21,21 @@ public class Champ {
     }
 
     public void init(int i, int j, int nbOfMines) {
-        //init field 
+        // init field
         this.field = new int[i][j];
         this.nbOfMines = nbOfMines;
-        for(int k=0; k<this.field.length; k++) {
-            for(int l=0; l<this.field[0].length; l++) {
+        for (int k = 0; k < this.field.length; k++) {
+            for (int l = 0; l < this.field[0].length; l++) {
                 this.field[k][l] = 0;
             }
         }
 
-        //place mines
+        // place mines
         int cpt = nbOfMines;
-        while(cpt > 0) {    
-            int x = (int)(Math.random() * i);
-            int y = (int)(Math.random() * j);
-            if(this.field[x][y] != -1) {
+        while (cpt > 0) {
+            int x = (int) (Math.random() * i);
+            int y = (int) (Math.random() * j);
+            if (this.field[x][y] != -1) {
                 this.field[x][y] = -1;
                 this.increseNeighbours(x, y);
                 cpt--;
@@ -51,13 +49,14 @@ public class Champ {
 
     public void endGame() {
         System.out.println("Game over!");
-    } 
+    }
 
-    public void increseNeighbours(int x, int y){
-        for(int k=-1; k<=1; k++) {
-            for(int l=-1; l<=1; l++) {
-                if(x+k >= 0 && x+k < this.field.length && y+l >= 0 && y+l < this.field[0].length && this.getVal(x+k, y+l) != -1) {
-                    this.field[x+k][y+l]++;
+    public void increseNeighbours(int x, int y) {
+        for (int k = -1; k <= 1; k++) {
+            for (int l = -1; l <= 1; l++) {
+                if (x + k >= 0 && x + k < this.field.length && y + l >= 0 && y + l < this.field[0].length
+                        && this.getVal(x + k, y + l) != -1) {
+                    this.field[x + k][y + l]++;
                 }
             }
         }
